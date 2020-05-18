35b31269cee5c91c9372262d8b63835439b0b6f9
talisonmaturana <talisondaniel@hotmail.com>
2020-05-18 15:12:12 -0300

    Removing credentials from database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 exports.connect = () => {
   mongoose.connect(
-    'mongodb+srv://talison:VcjKaxXUslnTSxS6@cluster0-ued3b.mongodb.net/test?retryWrites=true&w=majority',
+    'mongodb+srv://<your_user>:<your_password>@cluster0-ued3b.mongodb.net/test?retryWrites=true&w=majority',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
